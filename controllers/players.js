0546ffafc7dc90216aa2c40e984d509dcf64228b
CAhylA1 <cahylalouidor@gmail.com>
2024-08-21 19:53:01 -0400

    wip: updated

--- controllers/players.js
+++ controllers/players.js
@@ -1,3 +1,5 @@
+
+
 module.exports = {
     index
 }
@@ -8,12 +10,12 @@ async function index(req, res) {
         // Should have an Authorization header property with value of an API KEY
         method: "GET", 
         headers: {
-            Authorization: "e3b552d2-6624-4e7a-adbe-45f197dc4a3b"
+            Authorization: ` ${process.env.BALLDONTLIE_API_KEY}`
         }
         //Step 2 : Use fetch function with url and options objects 
         // step 3 : whatever data I get needs to get sent back as json data 
     }
     const player = await fetch("https://www.balldontlie.io/api/v1/players", options)
-    data = await res.json()
+    const data = await player.json()
     console.log(data)
 }
