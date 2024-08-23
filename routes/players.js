a9dd74eff53f5396ccb51b5c526f02b894d62a51
CAhylA1 <cahylalouidor@gmail.com>
2024-08-22 21:45:41 -0400

    wip: Updated Route

--- routes/players.js
+++ routes/players.js
@@ -1,7 +1,5 @@
 const express = require('express')
-const playersCtrl = require('../controllers/players')
 const router = express.Router()
 
-router.get('/', playersCtrl.index ) 
 
 module.exports = router
